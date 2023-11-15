efa18fca5a473e5024def7b60c31202095dddaf4
stack0801 <stack0801@gmail.com>
2023-11-15 17:37:34 +0900

    검색상자 임시 디자인

--- src/components/SearchBox.vue
+++ src/components/SearchBox.vue
@@ -1,3 +1,17 @@
+<template>
+  <div class="search-container">
+    <input
+      type="text"
+      v-model="keyword"
+      placeholder="검색어 입력"
+      class="search-input"
+    />
+    <button @click="submitSearch" class="search-button">
+      검색
+    </button>
+  </div>
+</template>
+
 <script setup>
 import { ref } from "vue";
 import { useRouter } from "vue-router";
@@ -12,11 +26,39 @@ const submitSearch = () => {
 };
 </script>
 
-<template>
-  <div>
-    <input type="text" v-model="keyword" placeholder="검색어 입력" />
-    <button @click="submitSearch">검색</button>
-  </div>
-</template>
+<style scoped>
+.search-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 10px;
+}
 
-<style scoped></style>
+.search-input {
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  width: 250px;
+  box-shadow: inset 0 1px 3px rgba(0, 0, 0, 0.1);
+  transition: border-color 0.3s;
+}
+
+.search-input:focus {
+  border-color: #007bff;
+  outline: none;
+}
+
+.search-button {
+  padding: 10px 20px;
+  background-color: #007bff;
+  color: white;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  transition: background-color 0.3s;
+}
+
+.search-button:hover {
+  background-color: #0056b3;
+}
+</style>
