1291056266783e2d6f3b294d5573887261a51b66
bohyeon9888 <bohyeon9888@gmail.com>
2023-11-16 23:26:25 +0900

    make plan view

--- src/views/PlanWriteView.vue
+++ src/views/PlanWriteView.vue
@@ -1,26 +1,73 @@
-<script setup></script>
+<script setup>
+import { reactive, ref } from "vue";
+
+const newEntry = reactive({
+  name: "",
+  date: "",
+  timeStart: "",
+  timeEnd: "",
+  description: "",
+});
+const entries = ref([]);
+
+function addEntry() {
+  console.log(newEntry);
+  entries.value.push({ ...newEntry });
+
+  // 폼 입력을 초기화
+  Object.keys(newEntry).forEach((key) => {
+    newEntry[key] = "";
+  });
+}
+</script>
 
 <template>
   <div class="container">
-    <form action="/submit" method="post">
-      <!-- form의 action과 method는 서버 설정에 맞게 수정하세요. -->
+    <div id="plan-box">
+      <h3>저장된 데이터</h3>
+      <div id="card-container" v-for="entry in entries" class="entry">
+        <p><strong>성함:</strong> {{ entry.name }}</p>
+        <p><strong>날짜:</strong> {{ entry.date }}</p>
+        <p><strong>시작 시간:</strong> {{ entry.timeStart }}</p>
+        <p><strong>종료 시간:</strong> {{ entry.timeEnd }}</p>
+        <p><strong>한줄메모:</strong> {{ entry.description }}</p>
+        <p><strong>태그:</strong> {{ entry.tag }}</p>
+        <p>==================================</p>
+      </div>
+    </div>
+    <form @submit.prevent="addEntry">
       <label for="name">성함</label>
-      <input type="text" id="name" name="name" /><br />
+      <input type="text" id="name" name="name" v-model="newEntry.name" /><br />
 
       <label for="date">날짜</label>
-      <input type="date" id="date" name="date" /><br />
+      <input type="date" id="date" name="date" v-model="newEntry.date" /><br />
 
       <label for="time_start">시작 시간</label>
-      <input type="time" id="time_start" name="time_start" />
+      <input
+        type="time"
+        id="time_start"
+        name="time_start"
+        v-model="newEntry.timeStart"
+      />
 
       <label for="time_end">종료 시간</label>
-      <input type="time" id="time_end" name="time_end" /><br />
+      <input
+        type="time"
+        id="time_end"
+        name="time_end"
+        v-model="newEntry.timeEnd"
+      /><br />
 
       <label for="description">한줄메모</label>
-      <input type="text" id="description" name="description" /><br />
+      <input
+        type="text"
+        id="description"
+        name="description"
+        v-model="newEntry.description"
+      /><br />
 
       <label for="tag">태그</label>
-      <input type="text" id="tag" name="tag" /><br />
+      <input type="text" id="tag" name="tag" v-model="newEntry.tag" /><br />
 
       <input type="submit" value="등록" />
     </form>
@@ -38,8 +85,9 @@
 }
 
 form,
-#map {
-  flex-basis: 48%; /* form과 map이 차지하는 공간의 비율을 설정합니다 */
+#map,
+#plan-box {
+  flex-basis: 30%; /* form과 map이 차지하는 공간의 비율을 설정합니다 */
   margin: 20px;
   padding: 20px;
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.1); /* 그림자 효과를 추가합니다 */
@@ -48,7 +96,7 @@ form,
 }
 
 #map {
-  height: 600px; /* 높이를 설정합니다 */
+  height: 500px; /* 높이를 설정합니다 */
   text-align: center;
   color: #888; /* 글자색을 설정합니다 */
 }
@@ -78,5 +126,9 @@ input[type="submit"] {
 
 input[type="submit"]:hover {
   background-color: #3949ab; /* 마우스를 올렸을 때 배경색을 변경합니다 */
+}
+
+#plan-box {
+  height: max-content;
 }
 </style>
