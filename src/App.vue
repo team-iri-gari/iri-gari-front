a1c036e177658aff79ace43c37dd79ffe39cf875
stack0801 <stack0801@gmail.com>
2023-11-16 15:31:36 +0900

    전역 스타일 수정

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup>
 
-import { RouterLink, RouterView } from 'vue-router'
+import {RouterView } from 'vue-router'
 import Header from './components/Header.vue';
 
 </script>
@@ -10,13 +10,6 @@ import Header from './components/Header.vue';
   <RouterView />
 </template>
 
-<style>
-
-@font-face {
-    font-family: 'YanoljaYacheR';
-    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_two@1.0/YanoljaYacheR.woff') format('woff');
-    font-weight: normal;
-    font-style: normal;
-}
+<style scoped>
 
 </style>
