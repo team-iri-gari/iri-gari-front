acfb5e881bf96cb612e374a7f3ad4991c62f9b7f
stack0801 <stack0801@gmail.com>
2023-11-17 17:30:06 +0900

    auth store 토큰 만료시 토큰 자동 삭제 기능 추가

--- src/stores/auth.js
+++ src/stores/auth.js
@@ -1,12 +1,38 @@
 import { defineStore } from 'pinia';
 
+
+// 토큰이 만료되었는지 확인
+function isTokenExpired(token) {
+  if (!token)
+    return true;
+
+  const payload = JSON.parse(atob(token.split('.')[1]));
+  return payload.exp * 1000 < Date.now();
+}
+
 export const useAuthStore = defineStore('auth', {
   state: () => ({
     token: localStorage.getItem('token') || null,
   }),
 
   getters: {
-    isAuthenticated: (state) => !!state.token,
+    isAuthenticated: (state) => {
+      if (isTokenExpired(state.token)) {
+        this.logout();
+        return false;
+      }
+      return true;
+    },
+
+    userData: (state) => {
+      if (isTokenExpired(state.token)) {
+        this.logout();
+        return null;
+      }
+
+      const payloadBase64 = state.token.split('.')[1];
+      return JSON.parse(atob(payloadBase64));
+    },
   },
 
   actions: {
@@ -18,9 +44,5 @@ export const useAuthStore = defineStore('auth', {
       this.token = null;
       localStorage.removeItem('token');
     },
-    getData() {
-      const payload = this.token.split('.')[1];
-      return JSON.parse(atob(payload))
-    }
   },
 });
