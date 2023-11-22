677b33852f101b95fb8a3bf4109664ce61dc9863
stack0801 <stack0801@gmail.com>
2023-11-22 14:51:28 +0900

    plan 도 카드보드로 변경

--- src/views/board/plan/ListView.vue
+++ src/views/board/plan/ListView.vue
@@ -1,10 +1,9 @@
 <script setup>
 import axios from "axios";
 import { ref, onMounted } from "vue";
-import { useRouter } from "vue-router";
+import CardBoard from '@/components/CardBoard.vue';
 
 const posts = ref([]);
-const router = useRouter();
 
 const getPosts = async () => {
   axios
@@ -18,10 +17,6 @@ const getPosts = async () => {
     });
 };
 
-const goToPost = (postId) => {
-  router.push(`/post/plan/${postId}`);
-};
-
 onMounted(() => {
   getPosts();
 });
@@ -31,67 +26,10 @@ onMounted(() => {
   <div>
     <h1>여행 기록</h1>
     <p><RouterLink to="/board/free">자유 게시판</RouterLink></p>
-    <router-link to="/write/plan">글작성</router-link>
-    <br />
-    <br />
-    <div class="card-container" v-for="item in posts" :key="item.articleId">
-      <div class="card-post" @click="goToPost(item.articleId)">
-        <div class="card-image"></div>
-        <div class="card-content">
-          <p class="card-title">{{ item.title }}</p>
-          <p class="card-name">{{ item.name }}</p>
-        </div>
-      </div>
-    </div>
+    <p><router-link to="/write/plan">글작성</router-link></p>
+    <CardBoard :cards="posts" :cardSize="'20vw'" />
   </div>
 </template>
 
 <style scoped>
-.card-container {
-  display: flex;
-  flex-direction: column;
-}
-
-.card-post {
-  display: flex;
-  align-items: center;
-  border: 1px solid #ddd;
-  border-radius: 10px;
-  margin-bottom: 10px;
-  padding: 10px;
-  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
-  transition: transform 0.3s ease, box-shadow 0.3s ease;
-  cursor: pointer;
-}
-
-.card-post:hover {
-  transform: translateY(-5px);
-  box-shadow: 0 4px 10px rgba(0, 0, 0, 0.2);
-}
-
-.card-image {
-  height: 60px;
-  width: 80px;
-  background: #666666;
-  border-radius: 5px;
-  margin-right: 10px;
-}
-
-.card-content {
-  display: flex;
-  flex-direction: column;
-}
-
-.card-title {
-  font-size: 18px;
-  font-weight: bold;
-  color: #333;
-  margin: 0;
-}
-
-.card-name {
-  font-size: 14px;
-  color: #666;
-  margin: 2px 0 0;
-}
 </style>
