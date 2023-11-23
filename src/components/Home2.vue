072ba6b54b8ff1e5df5b868637a68917d94ae1ea
stack0801 <stack0801@gmail.com>
2023-11-23 15:43:02 +0900

    사진이 이쁘지 않음

--- src/components/Home2.vue
+++ src/components/Home2.vue
@@ -34,9 +34,9 @@ const onSubmitSearch = (keyword) => {
 };
 
 const carouselItems = ref([
-    { imgSrc: '/images/carousel/1.webp', altText: 'First slide' },
-    { imgSrc: '/images/carousel/2.webp', altText: 'Second slide' },
-    { imgSrc: '/images/carousel/3.webp', altText: 'Third slide' }
+    { imgSrc: '/images/main-bg/bg01.jpg', altText: 'First slide' },
+    { imgSrc: '/images/main-bg/bg02.jpg', altText: 'Second slide' },
+    { imgSrc: '/images/main-bg/bg03.jpg', altText: 'Third slide' }
 ]);
 
 const props = defineProps({
