9a72e8f49e31988bd410e45f8a6406ab88e96832
bohyeon9888 <bohyeon9888@gmail.com>
2023-11-24 01:21:21 +0900

    modify time gap for background

--- src/components/Home2.vue
+++ src/components/Home2.vue
@@ -1,28 +1,44 @@
 <template>
-    <div id="container" class="carousel slide carousel-fade" data-bs-ride="carousel" :style="{ width, height }" ref="carousel">
-        <div class="carousel-inner">
-            <div v-for="(item, index) in carouselItems" :key="index" class="carousel-item" :class="{ active: index === 0 }"
-                data-bs-interval="3000">
-                <img :src="item.imgSrc" class="d-block w-100" :alt="item.altText" :style="{ width, height }">
-            </div>
-            <!-- overlay-content 위치 이동 필요 없음 -->
-        </div>
-        <div class="overlay-content">
-            <div class="logo-wrapper">
-                <div id="logo">
-                    <Logo />
-                </div>
-            </div>
-            <div id="banner">
-                <img src="/images/logo/irigari-en.png" style="height: 13dvh" />
-                <SearchBox :on-submit-search="onSubmitSearch" style="margin: auto" />
-            </div>
-        </div>
+  <div
+    id="container"
+    class="carousel slide carousel-fade"
+    data-bs-ride="carousel"
+    :style="{ width, height }"
+    ref="carousel"
+  >
+    <div class="carousel-inner">
+      <div
+        v-for="(item, index) in carouselItems"
+        :key="index"
+        class="carousel-item"
+        :class="{ active: index === 0 }"
+        data-bs-interval="2000"
+      >
+        <img
+          :src="item.imgSrc"
+          class="d-block w-100"
+          :alt="item.altText"
+          :style="{ width, height }"
+        />
+      </div>
+      <!-- overlay-content 위치 이동 필요 없음 -->
     </div>
+    <div class="overlay-content">
+      <div class="logo-wrapper">
+        <div id="logo">
+          <Logo />
+        </div>
+      </div>
+      <div id="banner">
+        <img src="/images/logo/irigari-en.png" style="height: 13dvh" />
+        <SearchBox :on-submit-search="onSubmitSearch" style="margin: auto" />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { ref } from "vue";
 import SearchBox from "@/components/SearchBox.vue";
 import Logo from "@/components/common/Logo.vue";
 import { useRouter } from "vue-router";
@@ -34,71 +50,71 @@ const onSubmitSearch = (keyword) => {
 };
 
 const carouselItems = ref([
-    { imgSrc: '/images/main-bg/bg01.jpg', altText: 'First slide' },
-    { imgSrc: '/images/main-bg/bg02.jpg', altText: 'Second slide' },
-    { imgSrc: '/images/main-bg/bg03.jpg', altText: 'Third slide' }
+  { imgSrc: "/images/main-bg/bg01.jpg", altText: "First slide" },
+  { imgSrc: "/images/main-bg/bg02.jpg", altText: "Second slide" },
+  { imgSrc: "/images/main-bg/bg03.jpg", altText: "Third slide" },
 ]);
 
 const props = defineProps({
-    width: {
-        type: String,
-        default: '100%'
-    },
-    height: {
-        type: String,
-        default: '100vh'
-    }
+  width: {
+    type: String,
+    default: "100%",
+  },
+  height: {
+    type: String,
+    default: "100vh",
+  },
 });
 </script>
 
 <style scoped>
 #container {
-    width: 100%;
-    height: 90vh;
-    position: relative;
+  width: 100%;
+  height: 90vh;
+  position: relative;
 }
 
 .carousel-item::after {
-    content: '';
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    top: 0;
-    left: 0;
-    background: rgba(0, 0, 0, 0.5);
-    backdrop-filter: blur(5px);
+  content: "";
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  background: rgba(0, 0, 0, 0.5);
+  backdrop-filter: blur(5px);
 }
 
 .carousel-item img {
-    opacity: 0.8;
-    filter: blur(2px);
+  opacity: 0.8;
+  filter: blur(2px);
 }
 
 .overlay-content {
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    z-index: 2;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 2;
 }
 
 .logo-wrapper {
-    display: table;
+  display: table;
 }
 
 #logo {
-    display: table-cell;
-    vertical-align: middle;
-    margin-left: 15px;
+  display: table-cell;
+  vertical-align: middle;
+  margin-left: 15px;
 }
 
 #banner {
-    justify-self: center;
-    margin: auto 0 auto 15px;
-    z-index: 16;
+  justify-self: center;
+  margin: auto 0 auto 15px;
+  z-index: 16;
 }
 </style>
